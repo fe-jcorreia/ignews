fd0f5477535b0ecac5e7afe9e2193127e289d21d
fernando <fe.jcorreia@gmail.com>
2021-06-25 13:17:42 -0300

    deploy bugs fixed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,7 +23,7 @@ export default function Home({ product }: HomeProps) {
         <section className={styles.hero}>
           <span>👏 Hey, welcome</span>
           <h1>
-            News about the <span>React</span> world.
+            News about the <span>React</span> world
           </h1>
           <p>
             Get access to all the publications <br />
